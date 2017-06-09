56e622164c859e10dfcccbb5871a0240e793458f
esmelinjehu <carlh12@gmail.com>
2017-06-09 12:33:14 -0500

    primer commit

---
+++ Prueba20/src/com/pe/proyecto/modelo/Alumno.java
@@ -0,0 +1,14 @@
+package com.pe.proyecto.modelo;
+
+public class Alumno {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
