0b363a08422999f76392fa98f50fad3a17234c76
esmelinjehu <carlh12@gmail.com>
2017-06-09 15:50:07 -0500

    se seteo el atributo dni

--- Prueba20/src/com/pe/proyecto/modelo/Alumno.java
+++ Prueba20/src/com/pe/proyecto/modelo/Alumno.java
@@ -2,6 +2,7 @@ package com.pe.proyecto.modelo;
 
 public class Alumno {
 	private String nombre;
+	
     private int dn1;
 	public String getNombre() {
 		return nombre;
@@ -10,5 +11,14 @@ public class Alumno {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	public int getDn1() {
+		return dn1;
+	}
+
+	public void setDn1(int dn1) {
+		this.dn1 = dn1;
+	}
+	
 
 }
